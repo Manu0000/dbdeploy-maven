4cfc556acc4d3b82e2eba80579802ab1464e6cc4
Manu0000 <45584019+Manu0000@users.noreply.github.com>
2019-06-11 08:23:22 +0530

    Add files via upload

---
+++ db/scripts/createSchemaVersionTable.sql
@@ -0,0 +1,6 @@
+create table if not exists changelog (
+  change_number bigserial primary key not null,
+  complete_dt timestamp with time zone not null,
+  applied_by text not null,
+  description text not null
+);
